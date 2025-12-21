22321de224f319829caef2404e4ad4e6762b6039
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:31:49 +0400

    add db configurations

---
+++ Auction.Infrastructure/Database/Configurations/AuctionItemConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Auction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auction.Infrastructure.Database.Configurations;
+
+public class AuctionItemConfiguration : IEntityTypeConfiguration<AuctionItem>
+{
+    public void Configure(EntityTypeBuilder<AuctionItem> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Name).IsRequired().HasMaxLength(200);
+        builder.Property(a => a.Description).IsRequired().HasMaxLength(500);
+
+        builder.Property(a => a.StartDate).IsRequired();
+
+        builder.Property(a => a.EndDate).IsRequired();
+
+        builder.Property(a => a.StartingPrice)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.Property(a => a.CurrentHighestBid).HasPrecision(18, 2);
+
+        builder.Property(a => a.Status).IsRequired();
+    }
+}
---
+++ Auction.Infrastructure/Database/Configurations/BidConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Auction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auction.Infrastructure.Database.Configurations;
+
+public class BidConfiguration : IEntityTypeConfiguration<Bid>
+{
+    public void Configure(EntityTypeBuilder<Bid> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Amount)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.Property(b => b.BidTime).IsRequired();
+
+        builder.HasOne(b => b.AuctionItem)
+            .WithMany(a => a.Bids)
+            .HasForeignKey(b => b.AuctionId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(b => b.UserId)
+            .IsRequired();
+    }
+}
