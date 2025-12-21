092a62ba37cbc97167797dea4bde4f1f6ae10d66
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:32:51 +0400

    add jwt service

---
+++ Auction.Application/Interfaces/Infrastructure/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿namespace Auction.Application.Interfaces.Infrastructure;
+
+public interface IJwtTokenService
+{
+    Task<string> CreateTokenAsync(
+        string userId,
+        string email,
+        IList<string> roles);
+}
---
+++ Auction.Infrastructure/Services/JwtTokenService.cs
@@ -0,0 +1,48 @@
+﻿using Auction.Application.Interfaces.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auction.Infrastructure.Services;
+
+public class JwtTokenService : IJwtTokenService
+{
+    private readonly IConfiguration _config;
+
+    public JwtTokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public Task<string> CreateTokenAsync(
+        string userId,
+        string email,
+        IList<string> roles)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId),
+            new Claim(JwtRegisteredClaimNames.Email, email)
+        };
+
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(60),
+            signingCredentials:
+                new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        );
+
+        return Task.FromResult(
+            new JwtSecurityTokenHandler().WriteToken(token));
+    }
+}
