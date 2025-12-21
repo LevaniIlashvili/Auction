d279a7b9818fdbc7062db4be8d6526466ee1d4a4
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:31:59 +0400

    add db context

---
+++ Auction.Infrastructure/Database/AuctionDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Auction.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Infrastructure.Database;
+
+public class AuctionDbContext : IdentityDbContext
+{
+    public DbSet<AuctionItem> AuctionItems { get; set; }
+    public DbSet<Bid> Bids { get; set; }
+
+    public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(AuctionDbContext).Assembly);
+    }
+}
