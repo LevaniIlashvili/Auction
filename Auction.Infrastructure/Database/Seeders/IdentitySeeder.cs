ee162c45f929f50d87861fc99cfd88d00fa7be4c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:32:42 +0400

    add seeder for user roles

---
+++ Auction.Infrastructure/Database/Seeders/IdentitySeeder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auction.Infrastructure.Database.Seeders;
+
+public static class IdentitySeeder
+{
+    public static async Task SeedAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] roles = { "User", "Admin" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
