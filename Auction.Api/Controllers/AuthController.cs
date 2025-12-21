4cf8cf6a14626337072cd11611c289da328accc6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:33:32 +0400

    add auth endpoints

---
+++ Auction.Api/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Auction.Application.Interfaces;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserIdentityService _identity;
+
+    public AuthController(IUserIdentityService identity)
+    {
+        _identity = identity;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        var result = await _identity.RegisterAsync(
+            request.Email, request.Password);
+
+        return result.Success ? Ok() : BadRequest(string.Join("", result.Errors));
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        var result = await _identity.LoginAsync(
+            request.Email, request.Password);
+
+        return result.Success
+            ? Ok(new { token = result.Token })
+            : Unauthorized("Invalid Credentials");
+    }
+}
