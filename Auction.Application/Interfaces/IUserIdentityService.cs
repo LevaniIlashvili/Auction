80b0a63ecc32aae07bab6bd926520b1f85b6b8da
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-21 18:32:59 +0400

    add identity service

---
+++ Auction.Application/Interfaces/IUserIdentityService.cs
@@ -0,0 +1,7 @@
+﻿namespace Auction.Application.Interfaces;
+
+public interface IUserIdentityService
+{
+    Task<(bool Success, IEnumerable<string> Errors)> RegisterAsync(string email, string password);
+    Task<(bool Success, string Token)> LoginAsync(string email, string password);
+}
---
+++ Auction.Infrastructure/Services/IdentityService.cs
@@ -0,0 +1,57 @@
+﻿using Auction.Application.Interfaces;
+using Auction.Application.Interfaces.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auction.Infrastructure.Services;
+
+public class IdentityService : IUserIdentityService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IJwtTokenService _jwtTokenService;
+
+    public IdentityService(UserManager<IdentityUser> userManager, IJwtTokenService jwtTokenService)
+    {
+        _userManager = userManager;
+        _jwtTokenService = jwtTokenService;
+    }
+
+    public async Task<(bool Success, IEnumerable<string> Errors)> RegisterAsync(
+       string email,
+       string password)
+    {
+        var user = new IdentityUser
+        {
+            UserName = email,
+            Email = email
+        };
+
+        var result = await _userManager.CreateAsync(user, password);
+
+        if (!result.Succeeded)
+            return (false, result.Errors.Select(e => e.Description));
+
+        var roleResult = await _userManager.AddToRoleAsync(user, "User");
+
+        if (!roleResult.Succeeded)
+            return (false, roleResult.Errors.Select(e => e.Description));
+
+        return (true, Array.Empty<string>());
+    }
+
+
+    public async Task<(bool Success, string Token)> LoginAsync(string email, string password)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+
+        if (user == null)
+            return (false, string.Empty);
+
+        if (!await _userManager.CheckPasswordAsync(user, password))
+            return (false, string.Empty);
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var token = await _jwtTokenService.CreateTokenAsync(user.Id, email, roles);
+
+        return (true, token);
+    }
+}
