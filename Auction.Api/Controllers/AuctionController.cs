b932bb27c1c01e1502acea647d624304536ab94c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-23 18:53:57 +0400

    add endpoints

--- Auction.Api/Controllers/AuctionController.cs
+++ Auction.Api/Controllers/AuctionController.cs
@@ -30,4 +30,20 @@ public class AuctionController : ControllerBase
 
         return Ok(auctions);
     }
+
+    [HttpGet("{auctionId}")]
+    public async Task<IActionResult> GetAuctionDetails([FromRoute] Guid auctionId, CancellationToken ct = default)
+    {
+        var auctionDetails = await _auctionService.GetAuctionDetailsAsync(auctionId, ct);
+
+        return Ok(auctionDetails);
+    }
+
+    [HttpPost("bid")]
+    public async Task<IActionResult> PlaceBid([FromBody] BidOnAuctionRequest request, CancellationToken ct = default)
+    {
+        await _auctionService.BidOnAuctionAsync(request, ct);
+
+        return Ok();
+    }
 }
