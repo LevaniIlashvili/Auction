3858662ab29e0c46c69ddce0ba8846e9343a6fea
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-27 18:12:35 +0400

    add auction hub

---
+++ Auction.Api/Hubs/AuctionHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Auction.Api.Hubs;
+
+public class AuctionHub : Hub
+{
+    public Task JoinAuction(Guid auctionId)
+    {
+        return Groups.AddToGroupAsync(Context.ConnectionId, auctionId.ToString());
+    }
+
+    public Task LeaveAuction(Guid auctionId)
+    {
+        return Groups.RemoveFromGroupAsync(Context.ConnectionId, auctionId.ToString());
+    }
+}
--- Auction.Api/Program.cs
+++ Auction.Api/Program.cs
@@ -61,6 +61,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapHub<AuctionHub>("/auctionHub");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
